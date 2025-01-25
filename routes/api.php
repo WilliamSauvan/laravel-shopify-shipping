e8a6ac5d204b6bef979baa5be997b1db5eb8ebcb
williamsauvan <wsauvan@webqam.fr>
2025-01-25 21:46:44 +0100

    feat(structure): fixing api config used

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post(
     '/shopify/shipping',
-    [config('shopify-shipping.callback_handler'), 'handle']
+    [config('laravel-shopify-shipping.callback_handler'), 'handle']
 )->name('shopify.shipping.callback');
